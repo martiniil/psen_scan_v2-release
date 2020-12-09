d413c5bd02711c021702956d6332d1630201304c
Alexander Gutenkunst <a.gutenkunst@pilz.de>
2020-12-09 14:08:24 +0100

    Imported upstream version '0.1.3' of 'upstream'

--- include/psen_scan_v2/scanner_state_machine.hpp
+++ include/psen_scan_v2/scanner_state_machine.hpp
@@ -63,7 +63,7 @@ template <class Event, class FSM>
 void ScannerProtocolDef::WaitForStartReply::on_exit(Event const&, FSM& fsm)
 {
   PSENSCAN_DEBUG("StateMachine", fmt::format("Exiting state: {}", "WaitForStartReply"));
-  // Stop watchdog...
+  // Stops the watchdog by resetting the pointer
   fsm.start_reply_watchdog_.reset();
 }
 
@@ -81,7 +81,7 @@ template <class Event, class FSM>
 void ScannerProtocolDef::WaitForMonitoringFrame::on_exit(Event const&, FSM& fsm)
 {
   PSENSCAN_DEBUG("StateMachine", fmt::format("Exiting state: {}", "WaitForMonitoringFrame"));
-  // Stop watchdog...
+  // Stops the watchdog by resetting the pointer
   fsm.monitoring_frame_watchdog_.reset();
 }
 
@@ -143,7 +143,8 @@ inline void ScannerProtocolDef::handleMonitoringFrame(const scanner_events::RawM
   PSENSCAN_DEBUG("StateMachine", "Action: handleMonitoringFrame");
   monitoring_frame_watchdog_->reset();
   const monitoring_frame::Message frame{ monitoring_frame::deserialize(event.data_, event.num_bytes_) };
-  PSENSCAN_WARN_THROTTLE(1 /* sec */, "StateMachine", "The scanner reports an error: {}", frame.diagnosticMessages());
+  PSENSCAN_WARN_THROTTLE(
+      1 /* sec */, "StateMachine", "The scanner reports an error: {}", formatRange(frame.diagnosticMessages()));
 
   printUserMsgFor(complete_scan_validator_.validate(frame, DEFAULT_NUM_MSG_PER_ROUND));
   args_->inform_user_about_laser_scan_cb(toLaserScan(frame));
